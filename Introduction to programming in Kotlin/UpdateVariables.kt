ec3d56648b5c1bb6d7717ed3e5dd27236dfc01a5
AlfiyaGR <120474577+AlfiyaGR@users.noreply.github.com>
2024-09-24 17:53:07 +0300

    Update UpdateVariables.kt

--- Introduction to programming in Kotlin/UpdateVariables.kt
+++ Introduction to programming in Kotlin/UpdateVariables.kt
@@ -1,7 +1,6 @@
 fun main() {
-    var cartTotal = 0
-    println("Total: $cartTotal")
-
-    cartTotal = 20
-    println("Total: $cartTotal")
+    var count = 10
+    println("You have $count unread messages.")
+    count = count + 1
+    println("You have $count unread messages.")
 }
