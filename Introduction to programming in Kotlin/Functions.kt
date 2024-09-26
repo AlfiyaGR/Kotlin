a412bae3570fb18a2a050d2559c5d876b4d76763
AlfiyaGR <120474577+AlfiyaGR@users.noreply.github.com>
2024-09-26 13:51:21 +0300

    Update Functions.kt

--- Introduction to programming in Kotlin/Functions.kt
+++ Introduction to programming in Kotlin/Functions.kt
@@ -1,6 +1,6 @@
 fun main() {
-    println(birthdayGreeting("Rover", 5))
-    println(birthdayGreeting("Rex", 2))
+    println(birthdayGreeting(age = 5, name = "Rover"))
+    println(birthdayGreeting(name = "Rex", age = 2))
 }
 
 fun birthdayGreeting(name: String, age: Int): String {
