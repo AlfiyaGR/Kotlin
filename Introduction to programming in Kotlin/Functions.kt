59aa5f3b944314dc905bc25bc7b959e7cc9440d6
AlfiyaGR <120474577+AlfiyaGR@users.noreply.github.com>
2024-09-26 14:17:36 +0300

    Update Functions.kt

--- Introduction to programming in Kotlin/Functions.kt
+++ Introduction to programming in Kotlin/Functions.kt
@@ -1,8 +1,16 @@
 fun main() {
-    println(birthdayGreeting(age = 5))
-    println(birthdayGreeting("Rex", 2))
+    val firstNumber = 10
+    val secondNumber = 5
+    val thirdNumber = 8
+    
+    val result = add(firstNumber, secondNumber)
+    val anotherResult = add(firstNumber, thirdNumber)
+
+    println("$firstNumber + $secondNumber = $result")
+    println("$firstNumber + $thirdNumber = $anotherResult")
 }
 
-fun birthdayGreeting(name: String = "Rover", age: Int): String {
-    return "Happy Birthday, $name! You are now $age years old!"
+// Define add() function below this line
+fun add(firstNumber: Int, secondNumber: Int): Int {
+    return firstNumber + secondNumber
 }
