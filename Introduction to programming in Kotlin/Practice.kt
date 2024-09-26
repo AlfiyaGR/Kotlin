25310ac20bc2714b90db5c894baa7c7f3e9579e0
AlfiyaGR <120474577+AlfiyaGR@users.noreply.github.com>
2024-09-26 14:34:21 +0300

    Update Practice.kt

--- Introduction to programming in Kotlin/Practice.kt
+++ Introduction to programming in Kotlin/Practice.kt
@@ -1,25 +1,11 @@
 fun main() {
-    val firstUserEmailId = "user_one@gmail.com"
-
-    // The following line of code assumes that you named your parameter as emailId. 
-    // If you named it differently, feel free to update the name.
-    println(displayAlertMessage(emailId = firstUserEmailId))
-    println()
-
-    val secondUserOperatingSystem = "Windows"
-    val secondUserEmailId = "user_two@gmail.com"
-
-    println(displayAlertMessage(secondUserOperatingSystem, secondUserEmailId))
-    println()
-
-    val thirdUserOperatingSystem = "Mac OS"
-    val thirdUserEmailId = "user_three@gmail.com"
-
-    println(displayAlertMessage(thirdUserOperatingSystem, thirdUserEmailId))
-    println()
+    val steps = 4000
+    val caloriesBurned = pedometrStepsToCalories(steps);
+    println("Walking $steps steps burns $caloriesBurned calories") 
 }
 
-// Define your displayAlertMessage() below this line.
-fun displayAlertMessage(operatingSystem: String = "Unknown OS", emailId: String): String{
-    return "There's a new sign-in request on $operatingSystem for your Google Account $emailId."
+fun pedometrStepsToCalories(numberOfSteps: Int): Double {
+    val caloriesBurnedForEachStep = 0.04
+    val totalCaloriesBurned = numberOfSteps * caloriesBurnedForEachStep
+    return totalCaloriesBurned
 }
