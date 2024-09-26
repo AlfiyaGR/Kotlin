720e2e59d2774dd6a9442c52995781bc1fda6e78
AlfiyaGR <120474577+AlfiyaGR@users.noreply.github.com>
2024-09-26 14:32:02 +0300

    Update Practice.kt

--- Introduction to programming in Kotlin/Practice.kt
+++ Introduction to programming in Kotlin/Practice.kt
@@ -1,7 +1,25 @@
 fun main() {
-    val firstNumber = 10
-    val secondNumber = 5
-    val result = firstNumber + secondNumber
-    
-    println("$firstNumber + $secondNumber = $result")
+    val firstUserEmailId = "user_one@gmail.com"
+
+    // The following line of code assumes that you named your parameter as emailId. 
+    // If you named it differently, feel free to update the name.
+    println(displayAlertMessage(emailId = firstUserEmailId))
+    println()
+
+    val secondUserOperatingSystem = "Windows"
+    val secondUserEmailId = "user_two@gmail.com"
+
+    println(displayAlertMessage(secondUserOperatingSystem, secondUserEmailId))
+    println()
+
+    val thirdUserOperatingSystem = "Mac OS"
+    val thirdUserEmailId = "user_three@gmail.com"
+
+    println(displayAlertMessage(thirdUserOperatingSystem, thirdUserEmailId))
+    println()
+}
+
+// Define your displayAlertMessage() below this line.
+fun displayAlertMessage(operatingSystem: String = "Unknown OS", emailId: String): String{
+    return "There's a new sign-in request on $operatingSystem for your Google Account $emailId."
 }
