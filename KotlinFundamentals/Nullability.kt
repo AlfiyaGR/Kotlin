05107c082db7fec8acab9fd6a0d061d41212c629
AlfiyaGR <120474577+AlfiyaGR@users.noreply.github.com>
2024-10-14 21:30:15 +0300

    Update Nullability.kt
    Write a nullable Int value

--- KotlinFundamentals/Nullability.kt
+++ KotlinFundamentals/Nullability.kt
@@ -1,7 +1,7 @@
 fun main() {
-    var favoriteActor: String? = "Sandra Oh"
-    println(favoriteActor)
-
-    favoriteActor = null
-    println(favoriteActor)
+    var number: Int? = 10
+    println(number)
+    
+    number = null
+    println(number)
 }
