42643a7485f3443e70a8daecec708fb149fe5f4a
AlfiyaGR <120474577+AlfiyaGR@users.noreply.github.com>
2024-10-15 14:34:04 +0300

    Update Classes.kt
    Override superclass methods from subclasses

--- KotlinFundamentals/Classes.kt
+++ KotlinFundamentals/Classes.kt
@@ -123,4 +123,6 @@ fun main() {
     var smartDevice: SmartDevice = SmartTvDevice("Android TV", "Entertainment")
     smartDevice.turnOn()
     
+    smartDevice = SmartLightDevice("Google Light", "Utility")
+    smartDevice.turnOn()
 }
