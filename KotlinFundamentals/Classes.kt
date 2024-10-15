1fc08ceb198958c4d3c27a4d142dbd2b5a198b5d
AlfiyaGR <120474577+AlfiyaGR@users.noreply.github.com>
2024-10-15 14:15:28 +0300

    Update Classes.kt
    Define the relationship between the SmartDevice superclass and its subclasses

--- KotlinFundamentals/Classes.kt
+++ KotlinFundamentals/Classes.kt
@@ -1,8 +1,8 @@
-class SmartDevice(val name: String, val category: String) {
+open class SmartDevice(val name: String, val category: String) {
 
     var deviceStatus = "online"
     
-    constructor(name: String, category: String, statusCode: Int) : this(name, category) {
+    constructor(name: String, category: String, statusCode: Int) : this(name, category)  {
         deviceStatus = when (statusCode) {
             0 -> "offline"
             1 -> "online"
@@ -16,6 +16,50 @@ class SmartDevice(val name: String, val category: String) {
 
     fun turnOff() {
         println("Smart device is turned off.")
+    }
+}
+
+class SmartTvDevice(deviceName: String, deviceCategory: String) :
+    SmartDevice(name = deviceName, category = deviceCategory) {
+    
+    var speakerVolume = 2
+        set(value) {
+            if (value in 0..100) {
+                field = value
+            }
+        }
+    
+    var channelNumber = 1
+        set(value) {
+            if (value in 0..200) {
+                field = value
+            }
+        }
+        
+    fun increaseSpeakerVolume() {
+        speakerVolume++
+        println("Speaker volume increased to $speakerVolume.")
+    }
+    
+    fun nextChannel() {
+        channelNumber++
+        println("Channel number increased to $channelNumber.")
+    }
+}
+    
+class SmartLightDevice(deviceName: String, deviceCategory: String) :
+    SmartDevice(name = deviceName, category = deviceCategory) {
+
+    var brightnessLevel = 0
+        set(value) {
+            if (value in 0..100) {
+                field = value
+            }
+        }
+
+    fun increaseBrightness() {
+        brightnessLevel++
+        println("Brightness increased to $brightnessLevel.")
     }
 }
 
