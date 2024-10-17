1bd93bc1a8d99121a2b366bb8821efe0d0307801
AlfiyaGR <120474577+AlfiyaGR@users.noreply.github.com>
2024-10-17 12:21:46 +0300

    Update Practice.kt
    Temperature converter

--- KotlinFundamentals/Practice.kt
+++ KotlinFundamentals/Practice.kt
@@ -1,21 +1,14 @@
 fun main() {
-    val child = 5
-    val adult = 28
-    val senior = 87
-    
-    val isMonday = true
-    
-    println("The movie ticket price for a person aged $child is \$${ticketPrice(child, isMonday)}.")
-    println("The movie ticket price for a person aged $adult is \$${ticketPrice(adult, isMonday)}.")
-    println("The movie ticket price for a person aged $senior is \$${ticketPrice(senior, isMonday)}.")
+    // Fill in the code.
 }
 
-fun ticketPrice(age: Int, isMonday: Boolean): Int {
-    // Fill in the code.
-    return when(age) {
-        in 0..12 -> 15
-        in 13..60 -> if (isMonday) 25 else 30
-        in 61..100 -> 20
-        else -> -1
-    }
+
+fun printFinalTemperature(
+    initialMeasurement: Double, 
+    initialUnit: String, 
+    finalUnit: String, 
+    conversionFormula: (Double) -> Double
+) {
+    val finalMeasurement = String.format("%.2f", conversionFormula(initialMeasurement)) // two decimal places
+    println("$initialMeasurement degrees $initialUnit is $finalMeasurement degrees $finalUnit.")
 }
