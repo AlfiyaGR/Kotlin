026006ebf3627bf40dbae8dea73da3ee1385baf5
AlfiyaGR <120474577+AlfiyaGR@users.noreply.github.com>
2024-10-17 12:09:25 +0300

    Update FunctionsLambda.kt
    Use functions as a data type

--- KotlinFundamentals/FunctionsLambda.kt
+++ KotlinFundamentals/FunctionsLambda.kt
@@ -1,9 +1,14 @@
+val trick = {
+    println("No treats!")
+}
+
+val treat: () -> Unit = {
+    println("Have a treat!")
+}
+
 fun main() {
     val trickFunction = trick
     trick()
     trickFunction()
-}
-
-val trick = {
-    println("No treats!")
+    treat()
 }
