e0938602a39b904b5e435293aa333522e7c9d916
AlfiyaGR <120474577+AlfiyaGR@users.noreply.github.com>
2024-10-17 12:20:17 +0300

    Update Practice.kt
    Movie-ticket price

--- KotlinFundamentals/Practice.kt
+++ KotlinFundamentals/Practice.kt
@@ -1,17 +1,15 @@
 fun main() {
-    val morningNotification = 51
-    val eveningNotification = 135
+    val child = 5
+    val adult = 28
+    val senior = 87
     
-    printNotificationSummary(morningNotification)
-    printNotificationSummary(eveningNotification)
+    val isMonday = true
+    
+    println("The movie ticket price for a person aged $child is \$${ticketPrice(child, isMonday)}.")
+    println("The movie ticket price for a person aged $adult is \$${ticketPrice(adult, isMonday)}.")
+    println("The movie ticket price for a person aged $senior is \$${ticketPrice(senior, isMonday)}.")
 }
 
-
-fun printNotificationSummary(numberOfMessages: Int) {
+fun ticketPrice(age: Int, isMonday: Boolean): Int {
     // Fill in the code.
-    if (numberOfMessages < 100) {
-        println("You have ${numberOfMessages} notifications.")
-    } else {
-        println("Your phone is blowing up! You have 99+ notifications.")
-    }
 }
