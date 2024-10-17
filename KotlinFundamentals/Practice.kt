ac222de4c10549737431c6362e952d5e590ea04b
AlfiyaGR <120474577+AlfiyaGR@users.noreply.github.com>
2024-10-17 12:19:50 +0300

    Update Practice.kt
    Mobile notifications Solution

--- KotlinFundamentals/Practice.kt
+++ KotlinFundamentals/Practice.kt
@@ -9,4 +9,9 @@ fun main() {
 
 fun printNotificationSummary(numberOfMessages: Int) {
     // Fill in the code.
+    if (numberOfMessages < 100) {
+        println("You have ${numberOfMessages} notifications.")
+    } else {
+        println("Your phone is blowing up! You have 99+ notifications.")
+    }
 }
