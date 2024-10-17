7698173f5fb8005170d3c2929db7de96ea20a485
AlfiyaGR <120474577+AlfiyaGR@users.noreply.github.com>
2024-10-17 12:26:54 +0300

    Update Practice.kt
    Internet profile

--- KotlinFundamentals/Practice.kt
+++ KotlinFundamentals/Practice.kt
@@ -1,20 +1,14 @@
 fun main() {    
-    val mySong = Song("Run", "BTS", 2015, 137_330_300)
-    mySong.printDescription()
-    println(mySong.isPopular)
+    val amanda = Person("Amanda", 33, "play tennis", null)
+    val atiqah = Person("Atiqah", 28, "climb", amanda)
+    
+    amanda.showProfile()
+    atiqah.showProfile()
 }
 
 
-class Song(
-    val title: String, 
-    val artist: String, 
-    val yearPublished: Int, 
-    val playCount: Int
-){
-    val isPopular: Boolean
-        get() = playCount >= 1000
-
-    fun printDescription() {
-        println("$title, performed by $artist, was released in $yearPublished.")
-    }   
+class Person(val name: String, val age: Int, val hobby: String?, val referrer: Person?) {
+    fun showProfile() {
+       // Fill in code 
+    }
 }
