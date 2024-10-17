fcac9bec0167a240aec0617ccb6103c316eb6ac9
AlfiyaGR <120474577+AlfiyaGR@users.noreply.github.com>
2024-10-17 12:29:39 +0300

    Update Practice.kt
    Foldable phones Solution

--- KotlinFundamentals/Practice.kt
+++ KotlinFundamentals/Practice.kt
@@ -1,5 +1,5 @@
-class Phone(var isScreenLightOn: Boolean = false){
-    fun switchOn() {
+open class Phone(var isScreenLightOn: Boolean = false){
+    open fun switchOn() {
         isScreenLightOn = true
     }
     
@@ -11,4 +11,30 @@ class Phone(var isScreenLightOn: Boolean = false){
         val phoneScreenLight = if (isScreenLightOn) "on" else "off"
         println("The phone screen's light is $phoneScreenLight.")
     }
+}
+
+class FoldablePhone(var isFolded: Boolean = true): Phone() {
+    override fun switchOn() {
+        if (!isFolded) {
+            isScreenLightOn = true
+        }
+    }
+    
+    fun fold() {
+        isFolded = true
+    }
+    
+    fun unfold() {
+        isFolded = false
+    }
+}
+
+fun main() {    
+    val newFoldablePhone = FoldablePhone()
+    
+    newFoldablePhone.switchOn()
+    newFoldablePhone.checkPhoneScreenLight()
+    newFoldablePhone.unfold()
+    newFoldablePhone.switchOn()
+    newFoldablePhone.checkPhoneScreenLight()
 }
