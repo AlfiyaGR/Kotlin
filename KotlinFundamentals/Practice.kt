3260b062490d7a0721687aca27d40cba97426759
AlfiyaGR <120474577+AlfiyaGR@users.noreply.github.com>
2024-10-17 12:29:59 +0300

    Update Practice.kt
    Special auction

--- KotlinFundamentals/Practice.kt
+++ KotlinFundamentals/Practice.kt
@@ -1,40 +1,12 @@
-open class Phone(var isScreenLightOn: Boolean = false){
-    open fun switchOn() {
-        isScreenLightOn = true
-    }
+fun main() {
+    val winningBid = Bid(5000, "Private Collector")
     
-    fun switchOff() {
-        isScreenLightOn = false
-    }
-    
-    fun checkPhoneScreenLight() {
-        val phoneScreenLight = if (isScreenLightOn) "on" else "off"
-        println("The phone screen's light is $phoneScreenLight.")
-    }
+    println("Item A is sold at ${auctionPrice(winningBid, 2000)}.")
+    println("Item B is sold at ${auctionPrice(null, 3000)}.")
 }
 
-class FoldablePhone(var isFolded: Boolean = true): Phone() {
-    override fun switchOn() {
-        if (!isFolded) {
-            isScreenLightOn = true
-        }
-    }
-    
-    fun fold() {
-        isFolded = true
-    }
-    
-    fun unfold() {
-        isFolded = false
-    }
-}
-
-fun main() {    
-    val newFoldablePhone = FoldablePhone()
-    
-    newFoldablePhone.switchOn()
-    newFoldablePhone.checkPhoneScreenLight()
-    newFoldablePhone.unfold()
-    newFoldablePhone.switchOn()
-    newFoldablePhone.checkPhoneScreenLight()
+class Bid(val amount: Int, val bidder: String)
+ 
+fun auctionPrice(bid: Bid?, minimumPrice: Int): Int {
+   // Fill in the code.
 }
