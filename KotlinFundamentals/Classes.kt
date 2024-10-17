07391a892836b2a26fc347f564d485a2796658c8
AlfiyaGR <120474577+AlfiyaGR@users.noreply.github.com>
2024-10-17 10:59:21 +0300

    Update Classes.kt
    Define property delegates

--- KotlinFundamentals/Classes.kt
+++ KotlinFundamentals/Classes.kt
@@ -1,4 +1,7 @@
-internal open class SmartDevice protected constructor (val name: String, val category: String) {
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
+
+open class SmartDevice(val name: String, val category: String) {
 
     var deviceStatus = "online"
         protected set
@@ -18,27 +21,17 @@ class SmartTvDevice(deviceName: String, deviceCategory: String) :
     SmartDevice(name = deviceName, category = deviceCategory) {
         
     override val deviceType = "Smart TV"
+        
+    private var speakerVolume by RangeRegulator(initialValue = 2, minValue = 0, maxValue = 100)
 
-    private var speakerVolume = 2
-        set(value) {
-            if (value in 0..100) {
-                field = value
-            }
-        }
-
-     private var channelNumber = 1
-        set(value) {
-            if (value in 0..200) {
-                field = value
-            }
-        }
+    private var channelNumber by RangeRegulator(initialValue = 1, minValue = 0, maxValue = 200)
 
     fun increaseSpeakerVolume() {
         speakerVolume++
         println("Speaker volume increased to $speakerVolume.")
     }
 
-    protected fun nextChannel() {
+    fun nextChannel() {
         channelNumber++
         println("Channel number increased to $channelNumber.")
     }
@@ -62,12 +55,7 @@ class SmartLightDevice(deviceName: String, deviceCategory: String) :
     
     override val deviceType = "Smart Light"
         
-    private var brightnessLevel = 0
-        set(value) {
-            if (value in 0..100) {
-                field = value
-            }
-        }
+    private var brightnessLevel by RangeRegulator(initialValue = 0, minValue = 0, maxValue = 100)
 
     fun increaseBrightness() {
         brightnessLevel++
@@ -131,6 +119,25 @@ class SmartHome(
     fun turnOffAllDevices() {
         turnOffTv()
         turnOffLight()
+    }
+}
+
+class RangeRegulator(
+    initialValue: Int,
+    private val minValue: Int,
+    private val maxValue: Int
+) : ReadWriteProperty<Any?, Int> {
+
+    var fieldData = initialValue
+
+    override fun getValue(thisRef: Any?, property: KProperty<*>): Int {
+        return fieldData
+    }
+
+    override fun setValue(thisRef: Any?, property: KProperty<*>, value: Int) {
+        if (value in minValue..maxValue) {
+            fieldData = value
+        }
     }
 }
 
