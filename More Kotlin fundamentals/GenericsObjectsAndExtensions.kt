e8f92cc61215974d221ab3dcc4c369f2db13ef0c
AlfiyaGR <120474577+AlfiyaGR@users.noreply.github.com>
2024-11-07 15:42:13 +0300

    Declare objects as companion objects

--- More Kotlin fundamentals/GenericsObjectsAndExtensions.kt
+++ More Kotlin fundamentals/GenericsObjectsAndExtensions.kt
@@ -1,3 +1,14 @@
+class Quiz {
+    val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
+    val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
+    val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
+
+    companion object StudentProgress {
+        var total: Int = 10
+        var answered: Int = 3
+    }
+}
+
 enum class Difficulty {
     EASY, MEDIUM, HARD
 }
@@ -8,15 +19,10 @@ data class Question<T>(
     val difficulty: Difficulty
 )
 
-object StudentProgress {
-    var total: Int = 10
-    var answered: Int = 3
-}
-
 fun main() {
     val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
     val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
     val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
     println(question1.toString())
-    println("${StudentProgress.answered} of ${StudentProgress.total} answered.")
+    println("${Quiz.answered} of ${Quiz.total} answered.")
 }
