7474cb84aacf021b0032267311f45a4fcf56e7b4
AlfiyaGR <120474577+AlfiyaGR@users.noreply.github.com>
2024-11-07 15:33:37 +0300

    Use generics

--- More Kotlin fundamentals/GenericsObjectsAndExtensions.kt
+++ More Kotlin fundamentals/GenericsObjectsAndExtensions.kt
@@ -1,17 +1,11 @@
-class NumericQuestion(
+class Question<T>(
     val questionText: String,
-    val answer: Int,
+    val answer: T,
     val difficulty: String
 )
 
-class TrueOrFalseQuestion(
-    val questionText: String,
-    val answer: Boolean,
-    val difficulty: String
-)
-
-class FillInTheBlankQuestion(
-    val questionText: String,
-    val answer: String,
-    val difficulty: String
-)
+fun main() {
+    val question1 = Question<String>("Quoth the raven ___", "nevermore", "medium")
+    val question2 = Question<Boolean>("The sky is green. True or false", false, "easy")
+    val question3 = Question<Int>("How many days are there between full moons?", 28, "hard")
+}
