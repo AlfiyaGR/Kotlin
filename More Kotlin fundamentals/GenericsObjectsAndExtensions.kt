dca40f22c691e7828c5229cb50eb71778edab196
AlfiyaGR <120474577+AlfiyaGR@users.noreply.github.com>
2024-11-07 16:02:18 +0300

    Add an interface

--- More Kotlin fundamentals/GenericsObjectsAndExtensions.kt
+++ More Kotlin fundamentals/GenericsObjectsAndExtensions.kt
@@ -1,18 +1,22 @@
-val Quiz.StudentProgress.progressText: String
-    get() = "${answered} of ${total} answered"
-
-fun Quiz.StudentProgress.printProgressBar() {
-    repeat(Quiz.answered) { print("▓") }
-    repeat(Quiz.total - Quiz.answered) { print("▒") }
-    println()
-    println(Quiz.progressText)
+interface ProgressPrintable {
+    val progressText: String
+    fun printProgressBar()
 }
 
-class Quiz {
+class Quiz : ProgressPrintable  {
+    override val progressText: String
+        get() = "${answered} of ${total} answered"
     val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
     val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
     val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
-
+	
+    override fun printProgressBar() {
+        repeat(answered) { print("▓") }
+        repeat(total - answered) { print("▒") }
+        println()
+        println(progressText)
+    }
+    
     companion object StudentProgress {
         var total: Int = 10
         var answered: Int = 3
@@ -30,5 +34,5 @@ data class Question<T>(
 )
 
 fun main() {
-    Quiz.printProgressBar()
+    Quiz().printProgressBar()
 }
