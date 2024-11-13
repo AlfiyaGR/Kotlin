b503bd0647ad98bf73a95fc54a0e9d9ff8a7f187
AlfiyaGR <120474577+AlfiyaGR@users.noreply.github.com>
2024-11-13 21:57:46 +0300

    Create Array.kt

---
+++ More Kotlin fundamentals/Array.kt
@@ -0,0 +1,7 @@
+fun main() {
+    val rockPlanets = arrayOf<String>(
+        "Mercury", "Venus", "Earth", "Mars")
+    val gasPlanets = arrayOf(
+        "Jupiter", "Saturn", "Uranus", "Neptune")
+    val solarSystem = rockPlanets + gasPlanets
+}
