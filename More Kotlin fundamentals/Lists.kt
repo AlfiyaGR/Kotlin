7981afb8a741667426ae175fe89ff90cc8da33c6
AlfiyaGR <120474577+AlfiyaGR@users.noreply.github.com>
2024-11-13 22:04:45 +0300

    Access elements from a list

--- More Kotlin fundamentals/Lists.kt
+++ More Kotlin fundamentals/Lists.kt
@@ -4,4 +4,8 @@ fun main() {
         "Saturn", "Uranus", "Neptune")
     
     println(solarSystem.size) 
+    println(solarSystem[2])
+    println(solarSystem.get(3))
+    println(solarSystem.indexOf("Earth"))
+    println(solarSystem.indexOf("Pluto"))
 }
