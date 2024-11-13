b0daace2c671f9104486c93e93c9914207bc5d58
AlfiyaGR <120474577+AlfiyaGR@users.noreply.github.com>
2024-11-13 22:05:50 +0300

    Iterate over list elements

--- More Kotlin fundamentals/Lists.kt
+++ More Kotlin fundamentals/Lists.kt
@@ -8,4 +8,8 @@ fun main() {
     println(solarSystem.get(3))
     println(solarSystem.indexOf("Earth"))
     println(solarSystem.indexOf("Pluto"))
+    
+    for (planet in solarSystem) {
+    	println(planet)
+	}
 }
