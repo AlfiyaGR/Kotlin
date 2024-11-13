d3cca774735ade6b971701a5f75acc450b029fdb
AlfiyaGR <120474577+AlfiyaGR@users.noreply.github.com>
2024-11-13 21:58:59 +0300

    Access an element in an array

--- More Kotlin fundamentals/Arrays.kt
+++ More Kotlin fundamentals/Arrays.kt
@@ -4,4 +4,13 @@ fun main() {
     val gasPlanets = arrayOf(
         "Jupiter", "Saturn", "Uranus", "Neptune")
     val solarSystem = rockPlanets + gasPlanets
+    
+    println(solarSystem[0])
+	println(solarSystem[1])
+	println(solarSystem[2])
+	println(solarSystem[3])
+	println(solarSystem[4])
+	println(solarSystem[5])
+	println(solarSystem[6])
+    println(solarSystem[7])
 }
