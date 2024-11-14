3073c95aa321e18b89acea03a72d57b85f448946
Alfa <alfa@example.com>
2024-11-14 10:10:48 +0300

    Create ArtWithTitle function

--- artspaceapp/src/main/java/com/example/artspaceapp/MainActivity.kt
+++ artspaceapp/src/main/java/com/example/artspaceapp/MainActivity.kt
@@ -1,23 +1,39 @@
 package com.example.artspaceapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.artspaceapp.ui.theme.TipTimeTheme
 
 class MainActivity : ComponentActivity() {
@@ -37,48 +53,85 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@SuppressLint("AutoboxingStateCreation")
 @Composable
 fun ArtSpaceMain() {
-    var result by remember {
+    val result by remember {
         mutableStateOf(0)
     }
     when (result) {
         1-> ArtWithTitle(
-            ArtPicture = R.drawable.edvardmunch,
-            ArtTitle = stringResource(R.string.TEdvardMunch),
-            ArtistName = stringResource(id = R.string.EdvardMunch)
+            artPicture = R.drawable.edvardmunch,
+            artTitle = stringResource(R.string.TEdvardMunch),
+            artistName = stringResource(id = R.string.EdvardMunch)
         )
         2-> ArtWithTitle(
-            ArtPicture = R.drawable.johannes,
-            ArtTitle = stringResource(R.string.TJohannes),
-            ArtistName = stringResource(id = R.string.JohannesVermeer)
+            artPicture = R.drawable.johannes,
+            artTitle = stringResource(R.string.TJohannes),
+            artistName = stringResource(id = R.string.JohannesVermeer)
         )
         3-> ArtWithTitle(
-            ArtPicture = R.drawable.georges,
-            ArtTitle = stringResource(R.string.TGeorgesSeurat),
-            ArtistName = stringResource(id = R.string.George)
+            artPicture = R.drawable.georges,
+            artTitle = stringResource(R.string.TGeorgesSeurat),
+            artistName = stringResource(id = R.string.George)
         )
         4-> ArtWithTitle(
-            ArtPicture = R.drawable.selfportrait,
-            ArtTitle = stringResource(R.string.TSelfPortrait),
-            ArtistName = stringResource(id = R.string.SelfPortrait)
+            artPicture = R.drawable.selfportrait,
+            artTitle = stringResource(R.string.TSelfPortrait),
+            artistName = stringResource(id = R.string.SelfPortrait)
         )
         5-> ArtWithTitle(
-            ArtPicture = R.drawable.eugenedelacroix,
-            ArtTitle = stringResource(R.string.TEugeneDelacroix),
-            ArtistName = stringResource(id = R.string.EugeneDelacroix)
+            artPicture = R.drawable.eugenedelacroix,
+            artTitle = stringResource(R.string.TEugeneDelacroix),
+            artistName = stringResource(id = R.string.EugeneDelacroix)
         )
         else -> ArtWithTitle(
-            ArtPicture = R.drawable.jean_antoine,
-            ArtTitle = stringResource(R.string.TJeanAntoine),
-            ArtistName = stringResource(id = R.string.JeanAntoine)
+            artPicture = R.drawable.jean_antoine,
+            artTitle = stringResource(R.string.TJeanAntoine),
+            artistName = stringResource(id = R.string.JeanAntoine)
         )
     }
 }
 
 @Composable
-fun ArtWithTitle(ArtPicture: Any, ArtTitle: Any, ArtistName: Any) {
-    TODO("Not yet implemented")
+fun ArtWithTitle(artPicture: Int, artTitle: String, artistName: String, modifier: Modifier = Modifier) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Image(
+            painter = painterResource(id = artPicture),
+            contentDescription = null,
+            modifier = Modifier
+                .padding(20.dp)
+                .height(300.dp)
+                .width(300.dp)
+                .border(border = BorderStroke(2.dp, Color.Gray), shape = RectangleShape)
+                .shadow(elevation = 4.dp)
+                .padding(10.dp)
+        )
+        Card(
+            modifier = Modifier
+                .padding(horizontal = 45.dp, vertical = 90.dp)
+
+        ){
+            Column(modifier = Modifier.padding(16.dp)) {
+                Text(
+                    text = artTitle,
+                    fontSize = 25.sp,
+                    textAlign = TextAlign.Start,
+                    lineHeight = 30.sp
+                )
+                Text(
+                    text = artistName,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = modifier.padding(top = 8.dp)
+                )
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
